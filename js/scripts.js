ab010a88ecfeb80c003dbbf4fef3a233d37a9714
Eric Crudup <briefcasemanx@gmail.com>
2022-01-13 09:54:02 -0800

    added a number input that displays different messages depending on number

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
+  $("form#age").submit( function() {
+    event.preventDefault();
+    const inputAge = parseInt( $("#age1").val() );
 
-
-
+    if (inputAge >= 18)
+    {
+      $("#declined").hide();
+      $("#approved").show();
+    }
+    else 
+    {
+      $("#approved").hide();
+      $("#declined").show();
+    }
+  });
 });
